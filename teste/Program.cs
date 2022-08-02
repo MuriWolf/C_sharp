3bd1c82238df2d0c8292a0ed6f088f6e4e4ec9c7
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2022-08-02 16:55:28 -0300

    Initial commit

---
+++ teste/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        public string input(string ask) {
+            Console.WriteLine(ask);
+            string response = Console.ReadLine();
+            Return response;
+        }
+        static void Main(string[] args)
+        {
+            string nome = "";
+            nome = input("Digite o nome: ");
+            Console.WriteLine(nome);
+        }
+
+    }
+
+}
